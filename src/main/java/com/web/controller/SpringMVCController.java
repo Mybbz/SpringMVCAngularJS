a04a8695267b85e93830b17e2934fcfc84130fb2
Ruicong Yuan <youzige@126.com>
2014-08-27 10:02:00 -0400

    Spring MVC+AngularJS
    This is a demo of spring MVC integrated with AngularJS, nothing
    difficult

---
+++ src/main/java/com/web/controller/SpringMVCController.java
@@ -0,0 +1,24 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SpringMVCController {
+
+	@RequestMapping(value="/angularJS.web", method=RequestMethod.GET)
+	public String printWelcome(ModelMap model){
+		return "angularJS";
+	}
+	
+	@RequestMapping(value="/springAngularJS.web", method=RequestMethod.GET, produces={"application/xml", "application/json"})
+	public @ResponseBody Person getPerson(){
+		Person person = new Person();
+		person.setFirstName("Barrick");
+		person.setLastName("Obama");
+		return person;
+	}
+}
